65e81fc5e34a540f5b6215ea334b170946f76a7c
Joakim <jmalmberg.web@gmail.com>
2019-04-16 09:15:23 +0200

    fixat med sass

--- loop-templates/content-portfolio.php
+++ loop-templates/content-portfolio.php
@@ -12,23 +12,19 @@ $bransch_query = new WP_query([
 		<div class="thumbnail text-center">
 
 			<?php $link = get_post_meta(get_the_id(), 'link', true);?>
-
-			<a href="<?php echo $link ?>" class="btn btn-primary">
 			
 				<?php if($link){?>				
 					<?php the_post_thumbnail('portfolio-thumbnail', ['class'=> 'img-fluid']); ?>				
 				<?php }?>
 
 				<h1><?php the_title(); ?></h1>
-				<p><?php the_excerpt(); ?></p>
 
-				<footer>
-					<?php the_terms(get_the_ID(),
-						'bransch',
-						__('Branch: ', 'understrap')
-					); ?>
-				</footer>
-			</a>
+				<?php the_terms(get_the_ID(),
+					'bransch',
+					__('Branch: ', 'understrap')
+				); ?>
+
+				<p><?php the_excerpt(); ?></p>
 		</div>
 	</div>
 </div>
